a323287ebcb97c9eea5fa0b5d6dc5260bb4a8ccd
tnlong1997 <tnlong1997@gmail.com>
2018-06-15 10:02:40 +0700

    Update index.js

--- casino-ethereum/src/js/index.js
+++ casino-ethereum/src/js/index.js
@@ -2,13 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Web3 from 'web3'
 import './../css/index.css'
+
 class App extends React.Component {
 	constructor(props) {
 		super(props)
 		this.state = {
 			lastWinner: 0,
-			timer: 0
+			numberOfBets: 0,
+			minimumBet: 0,
+			totalBet: 0,
+			maxAmountOfBets: 0,
 		}
+		if (typeof web3 != 'undefined') {
+			console.log("Using web3 detected from external source like Metamask")
+			this.web3 = new Web3(web3.currentProvider)
+		} else {
+			this.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
+		}
+		const MyContract = web3.eth.contract([here goes the ABI interface])
+		this.state.ContractInstance = MyContract.at("0x925d81c01d878899adbb7d38f84ce9d5284fa2e7")
 	}
 
 	voteNumber(number){
